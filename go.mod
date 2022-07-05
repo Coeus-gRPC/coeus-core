79764642e1a10c207b3250982812754646a4cacf
Yifan Huang <yifan.evan.huang@gmail.com>
2022-07-05 10:35:38 -0700

    Add functionality of calculating distribution

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 require (
 	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/montanaflynn/stats v0.6.6 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
--- internal/report/report.go
+++ internal/report/report.go
@@ -4,22 +4,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
+	"github.com/montanaflynn/stats"
 	"io/ioutil"
+	"strconv"
 	"time"
 )
 
 type Report struct {
-	TotalCallNum           int       `json:"totalCallNum"`
-	SuccessCallCount       int       `json:"successCallCount"`
-	ConcurrencyLevel       int       `json:"concurrencyLevel"`
-	TotalTimeConsumption   float64   `json:"totalTimeConsumption"`
-	AverageTimeConsumption float64   `json:"averageTimeConsumption"`
-	FastestTimeConsumption float64   `json:"fastestTimeConsumption"`
-	SlowestTimeConsumption float64   `json:"slowestTimeConsumption"`
-	TimeConsumptions       []float64 `json:"timeConsumptions"`
-	Distribution           []float64 `json:"distribution"`
-	RequestPerSecond       float64   `json:"requestPerSecond"`
-	Messages               []string  `json:"messages"`
+	TotalCallNum           int                `json:"totalCallNum"`
+	SuccessCallCount       int                `json:"successCallCount"`
+	ConcurrencyLevel       int                `json:"concurrencyLevel"`
+	TotalTimeConsumption   float64            `json:"totalTimeConsumption"`
+	AverageTimeConsumption float64            `json:"averageTimeConsumption"`
+	FastestTimeConsumption float64            `json:"fastestTimeConsumption"`
+	SlowestTimeConsumption float64            `json:"slowestTimeConsumption"`
+	TimeConsumptions       []float64          `json:"timeConsumptions"`
+	Distribution           map[string]float64 `json:"distribution"`
+	RequestPerSecond       float64            `json:"requestPerSecond"`
+	Messages               []string           `json:"messages"`
 }
 
 type Reporter struct {
@@ -59,6 +61,7 @@ func GenerateReport(reporters []Reporter, totalTime time.Duration, concurrencyLe
 	report.FastestTimeConsumption = smallestElement(report.TimeConsumptions)
 	report.SlowestTimeConsumption = largestElement(report.TimeConsumptions)
 	report.RequestPerSecond = float64(report.TotalCallNum) / totalTime.Seconds()
+	report.Distribution = calculatePercentile(report.TimeConsumptions)
 
 	return report
 }
@@ -103,4 +106,20 @@ func largestElement(array []float64) float64 {
 	}
 
 	return largestNumber
+}
+
+func calculatePercentile(distribution []float64) map[string]float64 {
+	println("Calculating percentiles")
+	distributionMap := make(map[string]float64)
+	disData := stats.LoadRawData(distribution)
+	percentiles := []float64{5.0, 10.0, 25.0, 50.0, 75.0, 90.0, 95.0, 99.0}
+
+	for _, v := range percentiles {
+		println("Calculating ", v)
+		percentile, _ := stats.Percentile(disData, v)
+		distributionMap[strconv.Itoa(int(v))] = percentile
+	}
+
+	println("Finish Calculating percentils")
+	return distributionMap
 }
